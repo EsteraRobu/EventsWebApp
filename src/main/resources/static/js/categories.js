503248613e6259e6d634571bee987857c30ce908
DESKTOP-MMKDHVN\Estera <estera.robu@gmail.com>
2020-06-18 02:25:45 +0300

    update search

--- src/main/resources/static/js/categories.js
+++ src/main/resources/static/js/categories.js
@@ -26,4 +26,40 @@ function myFunction() {
         }
     }
 }
+function myFunctionRecomanded() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("recomandedin");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("recomandedtab");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[5];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
 
+
+function myFunctionFav() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myFav");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myFavId");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[5];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }}
